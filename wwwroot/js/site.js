6922661555f779204892241451941d45c1acda3a
Mark Hollas <markhollas@Marks-MacBook-Air.local>
2023-02-28 20:32:27 +0900

    first commit- ported from aspnet to standard html

---
+++ wwwroot/js/site.js
@@ -0,0 +1,44 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var $nav = $("#nav-area");
+function toggleNavMenu(event){
+    $nav.toggleClass("collapse");
+}
+
+(function(){
+    const images = ["1594798379708.jpg", "1594798178517.jpg", "IMG_7787.JPG", "IMG_7786.JPG", "IMG_7783.JPG" ];
+    let imageIndex = 0
+    setInterval(()=>{
+        if(images.length-1>imageIndex)
+            imageIndex++
+        else
+            imageIndex = 0;
+
+        let image = images[imageIndex]
+        $("#slider").fadeOut(function(){
+            $(this).attr("src", `img/${image}`).fadeIn();
+        })
+    },1000*2)
+})();
+
+$(".nav-link").on("click", function(event){
+    let target = $(this).data("scroll-to");
+    let box = document.querySelector(`#${target}`).getBoundingClientRect();
+    let offset  = window.innerWidth > 768 ? 60 : 200;
+    //compensate for sticky header
+    let scrollTarget = box.top+window.scrollY-offset;
+    //this works because css property 'scroll-behavior' is set on html tag
+    window.scrollTo(0,scrollTarget);
+})
+
+let navbar = document.querySelector(".navbar-collapse");
+let navbaritems = document.querySelectorAll(".nav-item")
+navbaritems.forEach((item)=>{
+    item.addEventListener("click", (e)=>{
+        navbar.classList.remove("show");
+    })
+})
+
